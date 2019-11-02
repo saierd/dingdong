95c42a58531f4f4e1ffdf6c62e895a8bf48713b2
Daniel Saier <mail@danielsaier.de>
2019-11-02 19:27:19 +0100

    Make the beep tone quieter

--- src/system/beep.cpp
+++ src/system/beep.cpp
@@ -2,7 +2,7 @@
 
 #include "system/external_process.h"
 
-std::string const beepCommand = "play -n synth .1 sine C7";
+std::string const beepCommand = "play -n synth .1 sine C7 vol 0.02";
 
 void beep() {
     runExternalProcess(beepCommand);
