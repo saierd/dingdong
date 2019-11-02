ea217cc3f7ea2b3dd32d26cc9c9a7abfa4988e8a
Daniel Saier <mail@danielsaier.de>
2019-11-02 14:50:11 +0100

    Disable HDMI output on the Raspberry Pi 4

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -9,7 +9,18 @@ set -e
 DINGDONG_PATH="/opt/dingdong"
 cd "${DINGDONG_PATH}"
 
+# Power saving.
+# =============
+
+# Disable HDMI output on Raspberry Pi 4, which can generate a lot of heat. The 4K output also automatically raises the
+# clock of the GPU. See https://www.raspberrypi.org/documentation/configuration/config-txt/overclocking.md for more
+# information.
+# Note that this command will fail on earlier versions of the Raspberry Pi, but those don't have the critical 4K HDMI
+# output anyway.
+sudo raspi-config nonint do_pi4video V3 || true
+
 # GPIO setup.
+# ===========
 
 # Enable SPI for the RFID reader (0 means on).
 sudo raspi-config nonint do_spi 0
@@ -18,12 +29,14 @@ sudo raspi-config nonint do_spi 0
 sudo raspi-config nonint do_i2c 0
 
 # Screen setup.
+# =============
 
 sudo ./scripts/display/set_brightness.sh 100
 ./scripts/display/set_screensaver_timeout.sh 20
 ./scripts/display/force_on.sh
 
 # Audio Setup.
+# ============
 
 # It is necessary to export this variable to connect to the user's PulseAudio session from within a systemd service.
 export PULSE_RUNTIME_PATH="/run/user/$(id -u)/pulse/"
@@ -37,5 +50,7 @@ fi
 ./scripts/audio/set_volume.sh
 
 # Launch the dingdong GUI.
+# ========================
+
 # The PULSE_PROP environment variable enables PulseAudio echo cancellation.
 DISPLAY=:0 PULSE_PROP=\"filter.want=echo-cancel\" ./bin/dingdong
