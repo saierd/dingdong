097c1c757ffaa673f1bc832faa11889b67225460
Daniel Saier <mail@danielsaier.de>
2020-08-13 18:28:26 +0200

    Update the audio output device name
    In newer versions of Rasbian the onboard audio output has a different
    name in ALSA/PulseAudio.

--- scripts/audio/set_volume.sh
+++ scripts/audio/set_volume.sh
@@ -2,4 +2,12 @@
 
 # Set output volume to the maximum. The actual volume can be adjusted using the amplifier of the speaker.
 echo "Set default audio volume to maximum"
-pactl set-sink-volume alsa_output.platform-soc_audio.analog-mono 100%
+
+device_name="alsa_output.platform-bcm2835_audio.analog-mono"
+
+pactl list | grep "alsa_output.platform-soc_audio.analog-mono" &> /dev/null
+if [ $? == 0 ]; then
+    device_name="alsa_output.platform-soc_audio.analog-mono"
+fi
+
+pactl set-sink-volume "${device_name}" 100%
